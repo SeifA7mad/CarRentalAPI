46c0d9214c3d03a925541715ce880fb9893c7303
SeifA7mad <sauofa_ahmad@yahoo.com>
2021-12-26 20:48:53 +0200

    #

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
 
 app.listen(3000, () => {
